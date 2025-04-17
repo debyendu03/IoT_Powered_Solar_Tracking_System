cfd1efe8350b8e9705b3cfc1d8fb3854b6f860fc
Dip <debyendu03@gmail.com>
2025-04-17 16:19:14 +0530

    added wifi connectivity

--- IOT_ENABLE_SOLAR_TRACKING_SYSTEM.ino
+++ IOT_ENABLE_SOLAR_TRACKING_SYSTEM.ino
@@ -1,14 +1,18 @@
 #include "INA226Sensor.h"
 #include "servoControl.h"
+#include "wifiConnection.h"
+
+const char* ssid="realme 6i";
+const char* pass="password";
 
 void setup() {
   Serial.begin(115200);
   setupServos();
   initINA226();
+  wifiConnection(ssid,pass);
 }
 
 void loop() {
 INA226Data SensorData = getINA226Value();
-//delay(1500);
 trackLight();
 }
