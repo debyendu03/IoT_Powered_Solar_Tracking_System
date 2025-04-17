cdbf1be43ae3f5780d5f4ec380cde96298e6b1a5
Dip <debyendu03@gmail.com>
2025-04-17 22:56:47 +0530

    Optimize INA226 code: reduced size and cleaned up

--- INA226Sensor.cpp
+++ INA226Sensor.cpp
@@ -1,43 +1,27 @@
-#include "INA226Sensor.h"  // Include header to match functions declared in .h
+#include "INA226Sensor.h"
 #include <Wire.h>
 #include <INA226_WE.h>
 
 #define I2C_ADDRESS 0x40
 INA226_WE ina226(I2C_ADDRESS); 
 
-// Your function definitions go here
 void initINA226() {
   Wire.begin(21, 22);
   ina226.init();
   ina226.setResistorRange(0.1, 1.3); // 0.1 Ohm shunt, up to 1.3A
   ina226.setCorrectionFactor(0.93);
-  ina226.waitUntilConversionCompleted(); // ensure first read isn't zero
-  Serial.println("INA226 Initialized.");
+  ina226.waitUntilConversionCompleted();
 }
 
 INA226Data getINA226Value() {
   INA226Data data;
   data.shuntVoltage_mV = ina226.getShuntVoltage_mV();
   data.busVoltage_V = ina226.getBusVoltage_V();
-  data.loadVoltage_V = data.busVoltage_V+ (data.shuntVoltage_mV/1000);
+  data.loadVoltage_V = data.busVoltage_V+ (data.shuntVoltage_mV/1000.0);
   data.current_mA = ina226.getCurrent_mA();
   data.power_mW = ina226.getBusPower();
   data.overflow = ina226.overflow;
-
-  Serial.print("Shunt Voltage [mV]: "); Serial.println(data.shuntVoltage_mV, 6);
-  Serial.print("Bus Voltage [V]: "); Serial.println(data.busVoltage_V);
-  Serial.print("Load Voltage [V]: "); Serial.println(data.loadVoltage_V);
-  Serial.print("Current[mA]: "); Serial.println(data.current_mA, 6);
-  Serial.print("Bus Power [mW]: "); Serial.println(data.power_mW , 6);
-
-  if (!ina226.overflow) {
-    Serial.println("Values OK - no overflow");
-  } else {
-    Serial.println("Overflow! Choose higher current range");
-  }
-
-  Serial.println();
-
+  Serial.println(data.overflow ? "Overflow! Choose higher current range" : "Values OK - no overflow");
   return data;
 }
 
--- INA226Sensor.h
+++ INA226Sensor.h
@@ -1,5 +1,6 @@
 #ifndef INA226_SENSOR_H
 #define INA226_SENSOR_H
+
 typedef struct {
   float shuntVoltage_mV;
   float busVoltage_V;
@@ -9,6 +10,7 @@ typedef struct {
   bool overflow;
 
 }INA226Data;
+
 void initINA226();
 INA226Data getINA226Value();
 
