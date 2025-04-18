8e07fe3390157787c70bac3c2e905932f381f699
Dip <debyendu03@gmail.com>
2025-04-18 12:24:41 +0530

    added periodic data sending to Blynk

--- sendToBlynk.cpp
+++ sendToBlynk.cpp
@@ -1,21 +1,32 @@
 #define BLYNK_TEMPLATE_ID "TMPL3C-B-3N2V"
 #define BLYNK_TEMPLATE_NAME "IoT Enable Solar Tracking System"
 #define BLYNK_AUTH_TOKEN "_gDiltngBrVsI1t4cG8cduavXtdPwkWu"
+
 #include <BlynkSimpleEsp32.h>  
 #include <WiFi.h>
 #include "sendToBlynk.h" 
 
+// Timing variables
+unsigned long lastSendTime = 0;
+const unsigned long sendInterval = 12000; // this value is in milliseconds 1 sec=1000 ms
+
 void initWiFiAndBlynk(const char* auth, const char* ssid, const char* pass) {
-  WiFi.begin(ssid,pass);
+  WiFi.begin(ssid, pass);
   Blynk.begin(auth, ssid, pass);
 }
 
 void sendToBlynk() {
-  INA226Data sensorData = getINA226Value();
-  Blynk.virtualWrite(V0, sensorData.shuntVoltage_mV);
-  Blynk.virtualWrite(V1, sensorData.busVoltage_V);
-  Blynk.virtualWrite(V2, sensorData.loadVoltage_V);
-  Blynk.virtualWrite(V3, sensorData.current_mA);
-  Blynk.virtualWrite(V4, sensorData.power_mW);
-  Blynk.run();
+  Blynk.run(); 
+  //interval
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastSendTime >= sendInterval) {
+    lastSendTime = currentMillis;
+
+    INA226Data sensorData = getINA226Value();
+    Blynk.virtualWrite(V0, sensorData.shuntVoltage_mV);
+    Blynk.virtualWrite(V1, sensorData.busVoltage_V);
+    Blynk.virtualWrite(V2, sensorData.loadVoltage_V);
+    Blynk.virtualWrite(V3, sensorData.current_mA);
+    Blynk.virtualWrite(V4, sensorData.power_mW);
+  }
 }
