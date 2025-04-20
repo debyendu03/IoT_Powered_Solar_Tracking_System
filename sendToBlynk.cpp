97c56bd15e33194e5eab8f9053bc4ae064e0c1bb
Dip <debyendu03@gmail.com>
2025-04-20 14:32:34 +0530

    changed blynk's configarations

--- sendToBlynk.cpp
+++ sendToBlynk.cpp
@@ -1,6 +1,6 @@
 #define BLYNK_TEMPLATE_ID "TMPL3C-B-3N2V"
 #define BLYNK_TEMPLATE_NAME "IoT Enable Solar Tracking System"
-#define BLYNK_AUTH_TOKEN "_gDiltngBrVsI1t4cG8cduavXtdPwkWu"
+#define BLYNK_AUTH_TOKEN "_gDiltngBrVsI1t4cG8cduavXtdPwkWun"
 
 #include <BlynkSimpleEsp32.h>  
 #include <WiFi.h>
