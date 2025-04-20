ba520b640fda702085bee2e16e07a77740d7a6f1
Dip <debyendu03@gmail.com>
2025-04-20 22:55:09 +0530

    Added Blynk button control for data sending with adjutable interval (1-60 mins,default 30 sec)

--- sendToBlynk.cpp
+++ sendToBlynk.cpp
@@ -6,18 +6,24 @@
 #include <WiFi.h>
 #include "sendToBlynk.h" 
 
+bool sendDataEnabled = false;
 unsigned long lastSendTime = 0;
-unsigned long sendInterval = 60000; // Default interval: 1 minute (60000 milliseconds)
+unsigned long sendInterval = 30000; // Default: 30 seconds
 
+// Button on/off control (V5)
+BLYNK_WRITE(V5) {
+  int buttonState = param.asInt();
+  sendDataEnabled = (buttonState == 1);
+}
+
+// Interval control (V6) in minutes
 BLYNK_WRITE(V6) {
-  int intervalValue = param.asInt(); 
-  sendInterval = intervalValue * 60000; 
-
-  if (sendInterval < 60000) { 
-    sendInterval = 60000;
-  }
-  if (sendInterval > 3600000) {  
-    sendInterval = 3600000;
+  int minutes = param.asInt();
+  
+  if (minutes >= 1 && minutes <= 60) {
+    sendInterval = minutes * 60000UL; // convert minutes to milliseconds
+  } else {
+    sendInterval = 30000; // if somehow invalid, fallback to default 30 seconds
   }
 }
 
@@ -27,17 +33,20 @@ void initWiFiAndBlynk(const char* auth, const char* ssid, const char* pass) {
 }
 
 void sendToBlynk() {
-  Blynk.run();  
-  unsigned long currentMillis = millis();
-  
-  if (currentMillis - lastSendTime >= sendInterval) {
-    lastSendTime = currentMillis;
+  Blynk.run();
 
-    INA226Data sensorData = getINA226Value();
-    Blynk.virtualWrite(V0, sensorData.shuntVoltage_mV);
-    Blynk.virtualWrite(V1, sensorData.busVoltage_V);
-    Blynk.virtualWrite(V2, sensorData.loadVoltage_V);
-    Blynk.virtualWrite(V3, sensorData.current_mA);
-    Blynk.virtualWrite(V4, sensorData.power_mW);
+  if (sendDataEnabled) {
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - lastSendTime >= sendInterval) {
+      lastSendTime = currentMillis;
+
+      INA226Data sensorData = getINA226Value();
+      Blynk.virtualWrite(V0, sensorData.shuntVoltage_mV);
+      Blynk.virtualWrite(V1, sensorData.busVoltage_V);
+      Blynk.virtualWrite(V2, sensorData.loadVoltage_V);
+      Blynk.virtualWrite(V3, sensorData.current_mA);
+      Blynk.virtualWrite(V4, sensorData.power_mW);
+    }
   }
 }
